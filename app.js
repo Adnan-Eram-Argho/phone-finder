7a8cdc2284f1411fda29177df4959da0bc179d38
Your Name <adnaneramargho@gmail.com>
2022-03-01 15:30:43 +0600

    show phones done

--- app.js
+++ app.js
@@ -26,25 +26,25 @@ const loadApi = async (url) => {
 loadApi();
 const display = (phones) => {
     const showPhones = document.getElementById('show-phones');
-
+    showPhones.innerHTML = ''
     phones.forEach(phone => {
+
+
         const div = document.createElement('div');
         div.classList.add('col');
         console.log(phone)
         div.innerHTML = `
         <div class="card h-100" onclick = showDetails(${phone.slug})>
-        <img src="${phone.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${phone.phone_name}</h5>
-                <p class="card-text">${phone.slug}</p>
-        </div>
-    </div>
-        `
+            <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.brand}</h5>
+                    <p class="card-text">${phone.phone_name}</p>
+                    <button class="btn btn-danger ">details</button>
+            </div>
+           
+        </div>`
         showPhones.appendChild(div)
     });
-
-
-
 
 }
 
