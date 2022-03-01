628ef1945be0d9d2ecd9085c1936042511be2d90
Your Name <adnaneramargho@gmail.com>
2022-03-01 21:08:32 +0600

    most of the functionalitys done

--- app.js
+++ app.js
@@ -23,11 +23,13 @@ const loadApi = async (url) => {
     }
     display(datas.data)
 }
-loadApi();
+
 // displaying phones
 const display = (phones) => {
+    const showdetailssection = document.getElementById('phone-spec');
     const showPhones = document.getElementById('show-phones');
     showPhones.innerHTML = ''
+    showdetailssection.innerHTML = '';
     phones.forEach(phone => {
 
 
@@ -36,9 +38,11 @@ const display = (phones) => {
         console.log(phone)
         try {
             div.innerHTML = `
-    <div class="card h-100" >
+    <div class="card h-100 cards" >
+    <div class="d-flex justify-content-center mt-5">
         <img src="${phone.image}" class="card-img-top" alt="...">
-            <div class="card-body">
+        </div>
+            <div class="card-body d-flex justify-content-center flex-column">
                 <h5 class="card-title">${phone.brand}</h5>
                 <p class="card-text">${phone.phone_name}</p>
 
@@ -46,6 +50,7 @@ const display = (phones) => {
         </div>
        
     </div>`
+
         } catch (error) {
             console.log(error)
         }
@@ -64,5 +69,51 @@ const showDetails = async (id) => {
 }
 
 const showDetailsData = (phone) => {
+    const showdetailssection = document.getElementById('phone-spec');
+    showdetailssection.innerHTML = '';
+    let release;
     console.log(phone)
+    console.log(phone.name)
+
+    if (phone.releaseDate == '') {
+        release = 'No release date found'
+    } else {
+        release = phone.releaseDate;
+    }
+
+    const div = document.createElement('div')
+    div.innerHTML = `
+    <div>
+    <img src="${phone.image}" class="  w-25 img-fluid mx-auto" alt="">
+</div>
+<div>
+    <h3>name: <span>${phone.name}</span></h3>
+    <hr>
+    <h4>release date: <span>${release}</span></h4>
+    <hr>
+    <h4>chipset: ${phone.mainFeatures.chipSet} </h4>
+    <hr>
+    <h4>memory: <span>${phone.mainFeatures.memory}</span></h4>
+    <hr>
+    <h4>sensors: <span> ${phone.mainFeatures.sensors[0]}, ${phone.mainFeatures.sensors[1]}, ${phone.mainFeatures.sensors[2]}, ${phone.mainFeatures.sensors[3]}, ${phone.mainFeatures.sensors[4]}, ${phone.mainFeatures.sensors[5]}</span></h4>
+    <hr>
+    <h4>dsplay size: <span>${phone.mainFeatures.displaySize}</span></h4>
+    <hr>
+    <h4>storage: <span>${phone.mainFeatures.storage}</span></h4>
+    <hr>
+    <h4>wlan: <span>${phone.others.WLAN}</span></h4>
+    <hr>
+    <h4>bluetooth: <span>${phone.others.Bluetooth}</span></h4>
+    <hr>
+    <h4>Gps: <span>${phone.others.GPS}</span></h4>
+    <hr>
+    <h4>Nfc: <span>${phone.others.NFC}</span></h4>
+    <hr>
+    <h4>radio: <span>${phone.others.Radio}</span></h4>
+    <hr>
+    <h4>Usb: <span>${phone.others.USB}</span></h4>
+    <hr>
+</div>
+    `
+    showdetailssection.appendChild(div)
 }
